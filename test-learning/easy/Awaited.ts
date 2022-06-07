add1e3b62e3b9c0749c513fb616d75791a1b3d10
TODAY <1614696693@qq.com>
2022-06-07 20:43:26 +0800

    Awaited

---
+++ test-learning/easy/Awaited.ts
@@ -0,0 +1,19 @@
+/* _____________ Your Code Here _____________ */
+
+type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer P> ? P extends Promise<unknown> ? MyAwaited<P> : P : never
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type X = Promise<string>
+type Y = Promise<{ field: number }>
+type Z = Promise<Promise<string | number>>
+
+type cases = [
+  Expect<Equal<MyAwaited<X>, string>>,
+  Expect<Equal<MyAwaited<Y>, { field: number }>>,
+  Expect<Equal<MyAwaited<Z>, string | number>>,
+]
+
+// @ts-expect-error
+type error = MyAwaited<number>
