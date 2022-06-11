31cfd0007a0e6c03f9cf1622afe7f595cca353e2
TODAY <1614696693@qq.com>
2022-06-11 22:51:48 +0800

    Replace

---
+++ test-learning/medium/Replace.ts
@@ -0,0 +1,17 @@
+/* _____________ Your Code Here _____________ */
+
+type Replace<S extends string, From extends string, To extends string> = S extends `${infer A}${From}${infer C}` ? From extends '' ? S : `${A}${To}${C}` : S
+
+type aaa = Replace<'foobar', 'bar', 'foo'>
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Replace<'foobar', 'bar', 'foo'>, 'foofoo'>>,
+  Expect<Equal<Replace<'foobarbar', 'bar', 'foo'>, 'foofoobar'>>,
+  Expect<Equal<Replace<'foobarbar', '', 'foo'>, 'foobarbar'>>,
+  Expect<Equal<Replace<'foobarbar', 'bar', ''>, 'foobar'>>,
+  Expect<Equal<Replace<'foobarbar', 'bra', 'foo'>, 'foobarbar'>>,
+  Expect<Equal<Replace<'', '', ''>, ''>>,
+]
