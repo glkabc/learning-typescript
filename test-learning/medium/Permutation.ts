b52b6c142266b2257038176130a0a9d3ae482d4a
TODAY <1614696693@qq.com>
2022-06-13 21:34:12 +0800

    Permutation

---
+++ test-learning/medium/Permutation.ts
@@ -0,0 +1,20 @@
+/* _____________ Your Code Here _____________ */
+
+type Permutation<T, U = T> = [T] extends [never] ? [] : T extends U ? [T, ...Permutation<Exclude<U, T>>] : []
+
+/**
+ * 网上答案，只能看懂
+ * https://juejin.cn/post/7087745734694404104
+ */
+
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Permutation<'A'>, ['A']>>,
+  Expect<Equal<Permutation<'A' | 'B' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
+  Expect<Equal<Permutation<'B' | 'A' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
+  Expect<Equal<Permutation<never>, []>>,
+]
+
