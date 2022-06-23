2935c6b544f80ea3e32a5a848dd4232eb02dff8b
TODAY <1614696693@qq.com>
2022-06-23 20:11:34 +0800

    Zip

---
+++ test-learning/medium/Zip.ts
@@ -0,0 +1,15 @@
+/* _____________ Your Code Here _____________ */
+
+type Zip<T extends Array<unknown>, K extends Array<unknown>> = T extends [infer A, ...infer B] ? K extends [infer C, ...infer D] ? [[A, C], ...Zip<B, D>] : [] : []
+
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Zip<[], []>, []>>,
+  Expect<Equal<Zip<[1, 2], [true, false]>, [[1, true], [2, false]]>>,
+  Expect<Equal<Zip<[1, 2, 3], ['1', '2']>, [[1, '1'], [2, '2']]>>,
+  Expect<Equal<Zip<[], [1, 2, 3]>, []>>,
+  Expect<Equal<Zip<[[1, 2]], [3]>, [[[1, 2], 3]]>>,
+]
