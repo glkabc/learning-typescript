42b53a3eddd8b466bbd2299f16e566140ac7001a
TODAY <1614696693@qq.com>
2022-06-23 21:25:32 +0800

    Trunc

---
+++ test-learning/medium/Trunc.ts
@@ -0,0 +1,17 @@
+/* _____________ Your Code Here _____________ */
+
+type Trunc<T extends string | number> = T extends number ? `${T}` extends `${infer NA}.${infer NB}` ? NA : `${T}` : T extends `${infer SA}.${infer SB}` ? SA : `${T}`
+
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Trunc<0.1>, '0'>>,
+  Expect<Equal<Trunc<1.234>, '1'>>,
+  Expect<Equal<Trunc<12.345>, '12'>>,
+  Expect<Equal<Trunc<-5.1>, '-5'>>,
+  Expect<Equal<Trunc<'1.234'>, '1'>>,
+  Expect<Equal<Trunc<'-10.234'>, '-10'>>,
+  Expect<Equal<Trunc<10>, '10'>>,
+]
