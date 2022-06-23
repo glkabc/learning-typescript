7abe65eb2483e3f4587e509a899416950112e3a9
TODAY <1614696693@qq.com>
2022-06-23 21:17:07 +0800

    TrimRight

---
+++ test-learning/medium/TrimRight.ts
@@ -0,0 +1,18 @@
+/* _____________ Your Code Here _____________ */
+
+type TrimRight<S extends string> = S extends `${infer A}${' ' | '\n' | '\t'}` ? TrimRight<A> : S
+
+type aa = TrimRight<'   foo bar  \n\t '>
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<TrimRight<'str'>, 'str'>>,
+  Expect<Equal<TrimRight<'str '>, 'str'>>,
+  Expect<Equal<TrimRight<'str     '>, 'str'>>,
+  Expect<Equal<TrimRight<'     str     '>, '     str'>>,
+  Expect<Equal<TrimRight<'   foo bar  \n\t '>, '   foo bar'>>,
+  Expect<Equal<TrimRight<''>, ''>>,
+  Expect<Equal<TrimRight<'\n\t '>, ''>>,
+]
