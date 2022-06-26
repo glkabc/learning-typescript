af4e22cf7bbc14af5dde386e1a0943c1a96b5433
TODAY <1614696693@qq.com>
2022-06-26 22:28:30 +0800

    Readonly

---
+++ test-learning/easy/Readonly.ts
@@ -0,0 +1,23 @@
+/* _____________ Your Code Here _____________ */
+
+type MyReadonly<T> = {
+    readonly [P in keyof T]: P extends object ? MyReadonly<P> : T[P]
+  }
+  
+  
+  /* _____________ Test Cases _____________ */
+  import type { Equal, Expect } from '@type-challenges/utils'
+  
+  type cases = [
+    Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>,
+  ]
+  
+  interface Todo1 {
+    title: string
+    description: string
+    completed: boolean
+    meta: {
+      author: string
+    }
+  }
+  
