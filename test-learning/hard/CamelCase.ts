c3bf38ba86cc93a6e04505e9c8ae9d7598f48076
TODAY <1614696693@qq.com>
2022-06-28 21:07:55 +0800

    CamelCase

---
+++ test-learning/hard/CamelCase.ts
@@ -0,0 +1,18 @@
+/* _____________ Your Code Here _____________ */
+
+type CamelCase<S extends string> = S extends `${infer A}_${infer B}${infer C}` ? `${Lowercase<A>}${Uppercase<B>}${CamelCase<Lowercase<C>>}` : Lowercase<S>
+
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<CamelCase<'foobar'>, 'foobar'>>,
+  Expect<Equal<CamelCase<'FOOBAR'>, 'foobar'>>,
+  Expect<Equal<CamelCase<'foo_bar'>, 'fooBar'>>,
+  Expect<Equal<CamelCase<'foo_bar_hello_world'>, 'fooBarHelloWorld'>>,
+  Expect<Equal<CamelCase<'HELLO_WORLD_WITH_TYPES'>, 'helloWorldWithTypes'>>,
+  Expect<Equal<CamelCase<'-'>, '-'>>,
+  Expect<Equal<CamelCase<''>, ''>>,
+  Expect<Equal<CamelCase<'😎'>, '😎'>>,
+]
