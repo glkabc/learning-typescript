31bc5e858cfd8c8716a95f485463c34e0c7cf245
TODAY <1614696693@qq.com>
2022-06-28 20:52:17 +0800

    RequiredKeys

---
+++ test-learning/hard/RequiredKeys.ts
@@ -0,0 +1,16 @@
+/* _____________ Your Code Here _____________ */
+type GetRequired<T> = {
+    [P in keyof T as T[P] extends Required<T>[P] ? P : never]: T[P]
+}
+type RequiredKeys<T> = keyof GetRequired<T>
+
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<RequiredKeys<{ a: number; b?: string }>, 'a'>>,
+  Expect<Equal<RequiredKeys<{ a: undefined; b?: undefined }>, 'a'>>,
+  Expect<Equal<RequiredKeys<{ a: undefined; b?: undefined; c: string; d: null }>, 'a' | 'c' | 'd'>>,
+  Expect<Equal<RequiredKeys<{}>, never>>,
+]
