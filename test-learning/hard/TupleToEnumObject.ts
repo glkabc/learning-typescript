0077a66722c8077421fe41c09afd43e5c8fc62c7
TODAY <1614696693@qq.com>
2022-07-01 21:48:40 +0800

    Tuple to Enum Object

---
+++ test-learning/hard/TupleToEnumObject.ts
@@ -0,0 +1,63 @@
+/* _____________ Your Code Here _____________ */
+type UppercaseStringFirst<T> = T extends `${infer A}${infer B}` ? `${Uppercase<A>}${B}` : T
+type StrInArrIndex<T, S, L extends Array<number> = []> = T extends readonly [infer A, ...infer B] ? S extends A ? L['length'] : StrInArrIndex<B, S, [...L, 1]> : never
+
+type Enum<T extends readonly string[], N extends boolean = false> = {
+    readonly [P in T[number] as P extends string ? UppercaseStringFirst<P> : never]: N extends true ? StrInArrIndex<T, P> : P
+}
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+const OperatingSystem = ['macOS', 'Windows', 'Linux'] as const
+const Command = ['echo', 'grep', 'sed', 'awk', 'cut', 'uniq', 'head', 'tail', 'xargs', 'shift'] as const
+
+type cases = [
+  Expect<Equal<Enum<[]>, {}>>,
+  Expect<Equal<
+  Enum<typeof OperatingSystem>,
+  {
+    readonly MacOS: 'macOS'
+    readonly Windows: 'Windows'
+    readonly Linux: 'Linux'
+  }
+  >>,
+  Expect<Equal<
+  Enum<typeof OperatingSystem, true>,
+  {
+    readonly MacOS: 0
+    readonly Windows: 1
+    readonly Linux: 2
+  }
+  >>,
+  Expect<Equal<
+  Enum<typeof Command>,
+  {
+    readonly Echo: 'echo'
+    readonly Grep: 'grep'
+    readonly Sed: 'sed'
+    readonly Awk: 'awk'
+    readonly Cut: 'cut'
+    readonly Uniq: 'uniq'
+    readonly Head: 'head'
+    readonly Tail: 'tail'
+    readonly Xargs: 'xargs'
+    readonly Shift: 'shift'
+  }
+  >>,
+  Expect<Equal<
+  Enum<typeof Command, true>,
+  {
+    readonly Echo: 0
+    readonly Grep: 1
+    readonly Sed: 2
+    readonly Awk: 3
+    readonly Cut: 4
+    readonly Uniq: 5
+    readonly Head: 6
+    readonly Tail: 7
+    readonly Xargs: 8
+    readonly Shift: 9
+  }
+  >>,
+]
